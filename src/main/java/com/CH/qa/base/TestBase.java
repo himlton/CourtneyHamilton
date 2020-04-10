653be8083f3daaa76f238f237a444f17170aa32e
himlton <hiteshmangal@gmail.com>
2020-04-10 13:41:08 -0500

    added screenshots folder logic into Init

--- src/main/java/com/CH/qa/base/TestBase.java
+++ src/main/java/com/CH/qa/base/TestBase.java
@@ -22,16 +22,16 @@ public class TestBase {
 	public static Properties prop;
 	public static WebDriver driver;
 	public static String path;
-	
-//To launch browser
+
+	//To launch browser
 	public static void init() throws IOException, InterruptedException {
-		
+
 		prop = new Properties();
 		path = System.getProperty("user.dir");
 		FileInputStream fileInputSteream = new FileInputStream(path+"\\src\\main\\java\\com\\CH\\qa\\config\\config.properties");
 		prop.load(fileInputSteream);
 
-		
+
 		System.setProperty("webdriver.chrome.driver",path+prop.getProperty("Chrome_Driver"));
 		driver = new ChromeDriver();
 		driver.get(prop.getProperty("url"));
@@ -39,23 +39,18 @@ public class TestBase {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(util.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(util.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
-		
-	}
-	
-	//Function to take screenshot
-	public void takeScreenShot(String fileName) throws IOException {
-		Screenshot takeScreenShot = new AShot().takeScreenshot(driver);
 		File file = new File(path + "\\ScreenShots");
 		if(!(file.exists())) {
 			file.mkdir();
 		}
-		
-		
+
+	}
+
+	//Function to take screenshot
+	public void takeScreenShot(String fileName) throws IOException {
+		Screenshot takeScreenShot = new AShot().takeScreenshot(driver);
 		ImageIO.write(takeScreenShot.getImage(), "png", new File(path+"\\ScreenShots\\" + fileName + ".png"));
-		
 	}
-	
-	
-	}
+}
 
 
